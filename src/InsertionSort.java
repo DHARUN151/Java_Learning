8a45a46e78d263dc7d2290cd7858f595ac4f1b44
dharun151 <sdkkrr2006@gmail.com>
2026-06-30 22:02:24 +0530

    updated dbConnection

---
+++ src/InsertionSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        insertionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+}
