4bb01d39a592c4dc7fbfa1fd8eef5686ae92a240
dharun151 <sdkkrr2006@gmail.com>
2026-07-07 12:05:42 +0530

    practiced single linked list

--- src/LinkedLisst.java
+++
@@ -1,5 +0,0 @@
-public class LinkedLisst {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/com/dharun/Main.java
@@ -0,0 +1,21 @@
+package com.dharun;
+
+public class Main {
+    public static void main(String[] args) {
+    LinkedLisst ll = new LinkedLisst();
+        ll.insertFirst(10);
+        ll.insertFirst(9);
+        ll.insertFirst(8);
+        ll.insertFirst(7);
+        ll.insertLast(88);
+        ll.insert(33, 3);
+        ll.display();
+        System.out.println(ll.deleteFirst());
+        ll.display();
+        System.out.println(ll.deleteLast());
+        ll.display();
+        System.out.println(ll.delete(2));
+        ll.display();
+
+    }
+}
